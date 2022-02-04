aa0600edc3a0c6713f7b42472f1284fb94b4814e
Bright Amegbor <brightcaiusamegbor@gmail.com>
2022-02-04 01:46:15 +0000

    minor fixes

--- personal_doc/personal_doc/settings.py
+++ personal_doc/personal_doc/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+import environ
+
+env = environ.Env()
+
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s=vsj*3pu+b5_v2by4ec%jw=0#j_bf036jt=((l$e!(cle^%9y'
+SECRET_KEY = env('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -89,7 +95,7 @@ DATABASES = {
         'NAME': 'PersonalDoc',
         'ENFORCE_SCHEMA': False,
         'CLIENT': {
-            'host': 'mongodb+srv://brightamegbor:Sedem1mongo@cluster0.qp0lc.mongodb.net/PersonalDoc?retryWrites=true&w=majority'
+            'host': 'mongodb+srv://{}:{}@cluster0.qp0lc.mongodb.net/PersonalDoc?retryWrites=true&w=majority'.format(env('DB_USERNAME'), env('DB_PASSWORD'))
         }
     }
 }
